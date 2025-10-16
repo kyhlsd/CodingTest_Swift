e7b2289f8e86e5f45a531ace9df62ac000febdf7
kyhlsd <113406379+kyhlsd@users.noreply.github.com>
2025-10-16 16:21:02 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.07 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.swift
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.swift
@@ -1,16 +1,9 @@
 import Foundation
 
 func solution(_ a:Int, _ b:Int, _ c:Int) -> Int {
-    var dict: [Int: Int] = [:]
-    [a, b, c].forEach {
-        if var number = dict[$0] {
-            number += 1
-        } else {
-            dict[$0] = 1
-        }
-    }
+    let set = Set([a, b, c])
     
-    switch dict.count {
+    switch set.count {
         case 1:
             return (3 * a) * (3 * a * a) * (3 * a * a * a)
         case 2:
