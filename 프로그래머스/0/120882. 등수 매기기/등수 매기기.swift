1196a674eb0c29b87d09757e349ed796fa1aea19
kyhlsd <113406379+kyhlsd@users.noreply.github.com>
2025-10-21 23:51:03 +0900

    [level 0] Title: 등수 매기기, Time: 0.18 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.swift
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.swift
@@ -3,13 +3,12 @@ import Foundation
 func solution(_ score:[[Int]]) -> [Int] {
     let sorted = score
         .map { $0[0] + $0[1] }
-        .sorted { $0 > $1 }
-    
-    var result = [Int]()
-    for item in score {
-        if let index = sorted.firstIndex(of: item[0] + item[1]) {
-            result.append(index + 1)
+        .sorted {
+            $0 > $1
         }
-    }
-    return result
+    let rank = score
+        .map {
+            sorted.firstIndex(of: $0[0] + $0[1])! + 1
+        }
+    return rank
 }
