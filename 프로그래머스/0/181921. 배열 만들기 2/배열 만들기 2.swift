518b6bc18d2817083d073e1cb51a2a7e06b26bda
kyhlsd <113406379+kyhlsd@users.noreply.github.com>
2025-10-22 14:11:08 +0900

    [level 0] Title: 배열 만들기 2, Time: 1.06 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.swift
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.swift
@@ -1,19 +1,19 @@
 import Foundation
 
 func solution(_ l: Int, _ r: Int) -> [Int] {
-    var array = [[5]]
-    let digit = Int(log(Double(r)))
-    for i in 1...digit {
-        array.append(
-            array[i-1].map {
-                [$0 * 10, $0 * 10 + 5]
-            }
-                .reduce([], +)
-        )
+    var result: [Int] = []
+    var i = 1
+    
+    while true {
+        let binary = String(i, radix: 2)
+        let number = Int(binary.replacingOccurrences(of: "1", with: "5"))!
+        if number > r {
+            break
+        }
+        if number >= l {
+            result.append(number)
+        }
+        i += 1
     }
-    let result = array.reduce([], +).filter { $0 >= l && $0 <= r}
-    if result.isEmpty {
-        return [-1]
-    }
-    return result
+    return result.isEmpty ? [-1] : result
 }
