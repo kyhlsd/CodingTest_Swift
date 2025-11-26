892cfc23f82b0596afce998ef40811fc38237880
kyhlsd <113406379+kyhlsd@users.noreply.github.com>
2025-11-26 11:29:32 +0900

    [Bronze I] Title: 세로읽기, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.swift
@@ -0,0 +1,26 @@
+var input: [[String]] = []
+var maxLength = 0
+var answer = ""
+
+for _ in 1...5 {
+    let line = readLine()!.map { String($0) }
+    maxLength = max(maxLength, line.count)
+    input.append(line)
+}
+
+for i in 0..<5 {
+    let num = maxLength - input[i].count
+    if num > 0 {
+        for _ in 0..<num {
+            input[i].append("")
+        }
+    }
+}
+
+for i in 0..<maxLength {
+    for j in 0..<5 {
+        answer += input[j][i]
+    }
+}
+
+print(answer)
